36f6222deaf94406f85d3b4b57d6472497caef21
Mauro Leal <mauro.costa.12.j@hotmail.com>
2024-11-24 23:47:43 -0300

    final

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,4 +1,6 @@
-import { getTodosPosts, criarPost } from "../models/postModels.js";
+import fs from "fs";
+import { getTodosPosts, criarPost, atualizarPost } from "../models/postModels.js";
+import gerarDescricaoComGemini from "../services/germiniService.js";
 
 export async function listarPosts (req, res)
 {
@@ -10,6 +12,44 @@ export async function postarNovoPost(req, res) {
     const novoPost = req.body; //req = requisição body = Corpo
     try {
         const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+    } catch (error){
+        console.error(error.message);
+        res.status(500).json({"Erro":"Falha na requisição"});
+    }
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
+    }
+}
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `http://localhost:3000/${id}.png`;
+  
+    try {
+        const imageBuffer = fs.readFileSync(`uploads/${id}.png`);
+        const descricao = await gerarDescricaoComGemini(imageBuffer);
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: req.body.alt
+        }
+        const postCriado = await atualizarPost(id,post);
         res.status(200).json(postCriado);
     } catch (error){
         console.error(error.message);
--- src/models/postModels.js
+++ src/models/postModels.js
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js";
 const conexao = await conectarAoBanco(process.env.CONEXAO);
 
@@ -12,4 +14,11 @@ export async function criarPost(novoPost){
     const db = conexao.db("imersao-dev");
     const colecao = db.collection("posts");
     return colecao.insertOne(novoPost);
+}
+
+export async function atualizarPost(id, novoPost){
+    const db = conexao.db("imersao-dev");
+    const colecao = db.collection("posts");
+    const objectID = ObjectId.createFromHexString(id);
+    return colecao.updateOne({_id: new ObjectId(objectID)},{$set: novoPost});
 }
